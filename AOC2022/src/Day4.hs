20d07999555aaeeb02d8d4993f8d7ff429cbf7ba
Szymon Michniak <s.michniak@icloud.com>
2022-12-04 09:26:57 +0100

    Year 2022 - Day 4

---
+++ AOC2022/src/Day4.hs
@@ -0,0 +1,25 @@
+module Day4 where
+
+import Data.List.Split (splitOneOf)
+import Utils (DayInput, DayMain, DaySolution, readInt, standardMain)
+
+type InputType = [[Int]]
+
+fromBool :: Bool -> Int
+fromBool True = 1
+fromBool False = 0
+
+inputReader :: DayInput InputType
+inputReader s = map (map readInt . splitOneOf "-,") (lines s)
+
+solution1 :: DaySolution InputType
+solution1 = sum . map (\[x, y, z, w] -> fromBool (z >= x && w <= y || x >= z && y <= w))
+
+solution2 :: DaySolution InputType
+solution2 = sum . map (\[x, y, z, w] -> fromBool (x <= z && z <= y || x <= w && w <= y || z <= x && x <= w || z <= y && y <= w))
+
+main1 :: DayMain
+main1 = standardMain solution1 inputReader
+
+main2 :: DayMain
+main2 = standardMain solution2 inputReader
--- AOC2022/src/Solutions.hs
+++ AOC2022/src/Solutions.hs
@@ -5,8 +5,10 @@ import Utils (DayMain)
 import qualified Day1
 import qualified Day2
 import qualified Day3
+import qualified Day4
 
 solutions :: [DayMain]
 solutions = [Day1.main1, Day1.main2,
              Day2.main1, Day2.main2,
-             Day3.main1, Day3.main2]
+             Day3.main1, Day3.main2,
+             Day4.main1, Day4.main2]
