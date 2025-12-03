1df60a8675eaefd4541d2633c3ba56851875e2f4
Szymon Michniak <s.michniak@icloud.com>
2025-12-03 02:12:52 +0100

    Year 2025 - Day 2

--- AOC2025/src/day02.ts
+++ AOC2025/src/day02.ts
@@ -1,9 +1,66 @@
 import { parseLines } from "./utils";
 
-export function part1(input: string): any {
-    return "TODO";
+function getRange(range: string): [number, number] {
+    const [start, end] = range.split("-").map(Number);
+    return [start, end];
 }
 
-export function part2(input: string): any {
-    return "TODO";
+function parseLine(line: string): [number, number][] {
+    return line.split(",").map(getRange);
+}
+
+function isInRange(number: number, range: [number, number]): boolean {
+    return number >= range[0] && number <= range[1];
+}
+
+function isInAnyRange(number: number, ranges: [number, number][]): boolean {
+    return ranges.some((range) => isInRange(number, range));
+}
+
+function numDigits(number: number): number {
+    return (Math.log(number) * Math.LOG10E + 1) | 0;
+}
+
+function decimalShift(number: number, shift: number): number {
+    return number * Math.pow(10, shift);
+}
+
+export function part1(input: string): number | string {
+    const ranges = parseLine(parseLines(input)[0]);
+    const maxNum = Math.max(...ranges.flat());
+    const halfMaxNum = decimalShift(maxNum, -numDigits(maxNum) / 2);
+    let idSum = 0;
+
+    for (let i = 1; i < halfMaxNum; i++) {
+        const doubleNum = decimalShift(i, numDigits(i)) + i;
+        if (isInAnyRange(doubleNum, ranges)) {
+            idSum += doubleNum;
+        }
+    }
+
+    return idSum;
+}
+
+export function part2(input: string): number | string {
+    const ranges = parseLine(parseLines(input)[0]);
+    const maxNum = Math.max(...ranges.flat());
+    const maxNumDigits = numDigits(maxNum);
+    const halfMaxNum = decimalShift(maxNum, -maxNumDigits / 2);
+    const usedNums = new Set<number>();
+    let idSum = 0;
+
+    for (let i = 1; i < halfMaxNum; i++) {
+        const iDigits = numDigits(i);
+        let copiedNum = i;
+        for (let j = 2; j <= maxNumDigits; j++) {
+            copiedNum = decimalShift(copiedNum, iDigits) + i;
+
+            if (isInAnyRange(copiedNum, ranges) && !usedNums.has(copiedNum)) {
+                idSum += copiedNum;
+                usedNums.add(copiedNum);
+            }
+        }
+    }
+
+    return idSum;
 }
--- AOC2025/test/day02.test.ts
+++ AOC2025/test/day02.test.ts
@@ -2,13 +2,14 @@ import { part1, part2 } from "../src/day02";
 
 describe("Day 2", () => {
     const exampleInput = `
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
 `;
 
     test("Part 1 Example", () => {
-        expect(part1(exampleInput.trim())).toBe("TODO");
+        expect(part1(exampleInput.trim())).toBe(1227775554);
     });
 
     test("Part 2 Example", () => {
-        expect(part2(exampleInput.trim())).toBe("TODO");
+        expect(part2(exampleInput.trim())).toBe(4174379265);
     });
 });
